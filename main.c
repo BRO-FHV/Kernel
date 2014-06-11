f51b5f46c13042b45d2fbf0a9ffaffc8af3d764c
sla89 <skullsplitter@die-optimisten.net>
2014-06-11 10:50:32 +0200

    merged from main

--- main.c
+++ main.c
@@ -8,116 +8,68 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
-#include "kernel.h"
 #include <interrupt/dr_interrupt.h>
+#include <eth/dr_eth.h>
+#include <soc_AM335x.h>
+#include <cpu/hw_cpu.h>
+#include <eth/echo/dr_echo.h>
 #include <timer/dr_timer.h>
 #include <led/dr_led.h>
 #include <soc_AM335x.h>
 #include <cpu/hw_cpu.h>
-#include <scheduler.h>
-#include <mmu/sc_mmu.h>
+//#include <mmu/sc_mmu.h>
+#include <eth/broadcast/dr_broadcast.h>
+#include <mmu/mmu.h>
 
-/****************************************************************************/
-/*                      INTERNAL MACRO DEFINITIONS                          */
-/****************************************************************************/
-#define UART_THR_RHR_REG           (SOC_UART_0_REGS)
+#define LEN_IP_ADDR                        (4u)
+#define ASCII_NUM_IDX                      (48u)
 
-#define MAX_ACNT                   (1u)
-#define MAX_CCNT                   (1u)
+#define START_ADDR_DDR                     (0x80000000)
+#define START_ADDR_DEV                     (0x44000000)
+#define START_ADDR_OCMC                    (0x40300000)
+#define NUM_SECTIONS_DDR                   (512)
+#define NUM_SECTIONS_DEV                   (960)
+#define NUM_SECTIONS_OCMC                  (1)
 
-#define TX_BUFFER_SIZE             (26u)
+/******************************************************************************
+**                      INTERNAL VARIABLE DEFINITIONS
+*******************************************************************************/
+/* Page tables start must be aligned in 16K boundary */
+#pragma DATA_ALIGN(pageTable, MMU_PAGETABLE_ALIGN_SIZE);
+static volatile unsigned int pageTable[MMU_PAGETABLE_NUM_ENTRY];
 
-/* EDMA3 Event queue number. */
-#define EVT_QUEUE_NUM              (0u)
+static void MMUConfigAndEnable(void);
 
-/* PaRAM Set number for Dummy Transfer. */
-#define DUMMY_CH_NUM               (5u)
-
-/* UART Module Input Frequency. */
-#define UART_MODULE_INPUT_CLK      (48000000u)
-
-/* Baud Rate of UART for communication. */
-#define BAUD_RATE_115200           (115200u)
-
-#define TX_THRESHOLD               (1)
-#define RX_THRESHOLD               (1)
-#define START_ADDR_DDR             (0x80000000)
-#define START_ADDR_DEV             (0x44000000)
-#define START_ADDR_OCMC            (0x40300000)
-#define NUM_SECTIONS_DDR           (512)
-#define NUM_SECTIONS_DEV           (960)
-#define NUM_SECTIONS_OCMC          (1)
-
-#pragma DATA_ALIGN(pageTable, 16384);
-static volatile unsigned int pageTable[4 * 1024];
-
-void switchLED2ON() {
-	while (1) {
-		int i = GPIOPinRead(SOC_GPIO_1_REGS, LED2_PIN);
-		if (i == 0) {
-			uint32_t* x = (uint32_t*) 0x81200000;
-			*x = 5;
-			printf("x: %p, *x: %d, i: %d\n", x, *x, i);
-			LedOn2();
-		}
-	}
-}
-void switchLED2OFF() {
-	while (1) {
-		int i = GPIOPinRead(SOC_GPIO_1_REGS, LED2_PIN);
-		if (i != 0) {
-			uint32_t* x = (uint32_t*) 0x81200000;
-			*x = 10;
-			printf("x: %p, *x: %d, i: %d\n", x, *x, i);
-			LedOff2();
-		}
-	}
-}
-
-void switchLED3ON() {
-	while (1) {
-		int i = GPIOPinRead(SOC_GPIO_1_REGS, LED3_PIN);
-		if (i == 0) {
-			LedOn3();
-		}
-	}
-}
-void switchLED3OFF() {
-	while (1) {
-		int i = GPIOPinRead(SOC_GPIO_1_REGS, LED3_PIN);
-		if (i != 0) {
-			LedOff3();
-		}
-	}
-}
 
 int main(void) {
-
 	CPUirqd();
-
-	MmuInit();
-
-	printf("config timer\n");
+//	MmuInit();
+    MMUConfigAndEnable();
 
 	IntControllerInit();
 
-	LedInitRegister();
-	LedInit2();
-	LedInit3();
+	//ALWAYS CONFIGURE DELAY TIMER FIRST!
+	//AND ENABLE INTERRUPTS AFTERWARDS
+	TimerDelaySetup();
 
-	TimerConfiguration(Timer_TIMER2, 20000, SchedulerRunNextProcess);
-	TimerEnable(Timer_TIMER2);
+//	TimerConfiguration(Timer_TIMER2, 20000, SchedulerRunNextProcess);
+//	TimerEnable(Timer_TIMER2);
+	//SchedulerStartProcess(&test);7
 
-	SchedulerStartProcess(&switchLED2ON);
-	SchedulerStartProcess(&switchLED2OFF);
-	SchedulerStartProcess(&switchLED3ON);
-	SchedulerStartProcess(&switchLED3OFF);
-
-	printf("end config start interrupt\n");
 	CPUirqe();
+
+	//uint32_t ipAddr = EthConfigureWithIP(0xC0A80007u); //0xC0A80007u => 192.168.0.7
+	uint32_t ipAddr = EthConfigureWithDHCP();
+	if(0 != ipAddr) {
+		printf("starting echo server...\n");
+		EchoStart();
+
+//		printf("starting broadcast service... \n");
+//		BroadcastStart();
+	} else {
+		printf("Ethernet setup failed... ");
+	}
 
 	while (1) {
 		volatile int i = 0;
@@ -125,6 +77,11 @@ int main(void) {
 		for (i = 0; i < 1000000; i++) {
 		}
 	}
+}
+
+#pragma INTERRUPT(fiq_handler, FIQ)
+interrupt void fiq_handler() {
+	printf("fiq interrupt\n");
 }
 
 /**
@@ -135,15 +92,62 @@ interrupt void udef_handler() {
 	printf("udef interrupt\n");
 }
 
-#pragma INTERRUPT(fiq_handler, FIQ)
-interrupt void fiq_handler() {
-	printf("fiq interrupt\n");
-}
-
 /**
  * Is called on any prefetch abort.
  */
 #pragma INTERRUPT(pabt_handler, PABT)
 interrupt void pabt_handler() {
 	printf("pabt interrupt\n");
+}
+
+void MMUConfigAndEnable(void)
+{
+    /*
+    ** Define DDR memory region of AM335x. DDR can be configured as Normal
+    ** memory with R/W access in user/privileged modes. The cache attributes
+    ** specified here are,
+    ** Inner - Write through, No Write Allocate
+    ** Outer - Write Back, Write Allocate
+    */
+    REGION regionDdr = {
+                        MMU_PGTYPE_SECTION, START_ADDR_DDR, NUM_SECTIONS_DDR,
+                        MMU_MEMTYPE_NORMAL_NON_SHAREABLE(MMU_CACHE_WT_NOWA,
+                                                         MMU_CACHE_WB_WA),
+                        MMU_REGION_NON_SECURE, MMU_AP_PRV_RW_USR_RW,
+                        (unsigned int*)pageTable
+                       };
+    /*
+    ** Define OCMC RAM region of AM335x. Same Attributes of DDR region given.
+    */
+    REGION regionOcmc = {
+                         MMU_PGTYPE_SECTION, START_ADDR_OCMC, NUM_SECTIONS_OCMC,
+                         MMU_MEMTYPE_NORMAL_NON_SHAREABLE(MMU_CACHE_WT_NOWA,
+                                                          MMU_CACHE_WB_WA),
+                         MMU_REGION_NON_SECURE, MMU_AP_PRV_RW_USR_RW,
+                         (unsigned int*)pageTable
+                        };
+
+    /*
+    ** Define Device Memory Region. The region between OCMC and DDR is
+    ** configured as device memory, with R/W access in user/privileged modes.
+    ** Also, the region is marked 'Execute Never'.
+    */
+    REGION regionDev = {
+                        MMU_PGTYPE_SECTION, START_ADDR_DEV, NUM_SECTIONS_DEV,
+                        MMU_MEMTYPE_STRONG_ORD_SHAREABLE,
+                        MMU_REGION_NON_SECURE,
+                        MMU_AP_PRV_RW_USR_RW  | MMU_SECTION_EXEC_NEVER,
+                        (unsigned int*)pageTable
+                       };
+
+    /* Initialize the page table and MMU */
+    MMUInit((unsigned int*)pageTable);
+
+    /* Map the defined regions */
+    MMUMemRegionMap(&regionDdr);
+    MMUMemRegionMap(&regionOcmc);
+    MMUMemRegionMap(&regionDev);
+
+    /* Now Safe to enable MMU */
+    MMUEnable((unsigned int*)pageTable);
 }
