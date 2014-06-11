557a8602d71ef02eb558d9c7b8b5bd0e60bf35b5
Johannes Wachter <johannes.wachter@students.fhv.at>
2014-06-11 09:34:50 +0200

    removed release files

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/Users/GIGI/GitWorkspace/BRO-FHV/Hal/Debug/Hal.lib" -l"C:/Users/GIGI/GitWorkspace/BRO-FHV/Driver/Debug/Driver.lib" -l"C:/Users/GIGI/GitWorkspace/BRO-FHV/Scheduler/Debug/Scheduler.lib"
-
