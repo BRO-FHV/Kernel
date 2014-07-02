2cdf1ec8084345393a93945262ee283ae4d16295
sla89 <skullsplitter@die-optimisten.net>
2014-07-02 09:41:53 +0200

    arschloch

--- main.c
+++ main.c
@@ -26,83 +26,6 @@
 #include <Led/lib_led.h>
 #include <stdlib/stdlib.h>
 
-#define PORT					2000
-
-uint8_t BROADCAST_IP[] = { 255, 255, 255, 255 };
-
-#define LENGTH_USERNAME 		7 //Byte
-#define CMD_LED					1
-
-typedef struct {
-	uint8_t username[LENGTH_USERNAME];
-	uint8_t cmd;
-} tCmd;
-
-typedef struct {
-	tCmd cmd;
-	uint8_t led;
-	uint8_t state;
-} tCmd_Led;
-
-/*
- * main.c
- */
-void Eth() {
-	SwiUdpInit(PORT);
-
-	while (1) {
-		if (SwiUdpHasData(PORT)) {
-			swi_udp_package_t* package = SwiUdpGetData(PORT);
-			tCmd* command = (tCmd*) (package->data);
-
-			if (CMD_LED == command->cmd) {
-				tCmd_Led* ledCmd = (tCmd_Led*) (package->data);
-
-				if (ledCmd->led >= 1 && ledCmd->led <= 4) {
-					switch (ledCmd->led) {
-					case 1:
-						if (ledCmd->state) {
-							lib_led_on_0();
-						} else {
-							lib_led_off_0();
-						}
-						break;
-					case 2:
-						if (ledCmd->state) {
-							lib_led_on_1();
-						} else {
-							lib_led_off_1();
-						}
-						break;
-					case 3:
-						if (ledCmd->state) {
-							lib_led_on_2();
-						} else {
-							lib_led_off_2();
-						}
-						break;
-					case 4:
-						if (ledCmd->state) {
-							lib_led_on_3();
-						} else {
-							lib_led_off_3();
-						}
-						break;
-					}
-
-					//BROADCAST TO ALL LISTENERS
-					SwiUdpSendData(BROADCAST_IP, PORT, package->data,
-							package->len);
-				}
-			}
-
-			lib_free(package->data);
-			package->data = NULL;
-			package->len = 0;
-		}
-	}
-}
-
 int main(void) {
 	CPUirqd();
 	MmuInit();
@@ -122,25 +45,22 @@ int main(void) {
 	CPUirqe();
 
 	// load elf
-//	startFileSystem();
-//	FILINFO fi;
-//
-//	if (f_stat("BRO_UDP.out", &fi) == FR_OK) {
-//		uint8_t* dataBuff = malloc(fi.fsize);
-//		getElfFile(dataBuff, fi.fsize, "BRO_UDP.out");
-//
-//		// start a process
-//		loadProcessFromElf(0, dataBuff);
-//		free(dataBuff);
-//	}
+	startFileSystem();
+	FILINFO fi;
 
-	CPUirqe();
+	if (f_stat("BRO_UDP.out", &fi) == FR_OK) {
+		uint8_t* dataBuff = malloc(fi.fsize);
+		getElfFile(dataBuff, fi.fsize, "BRO_UDP.out");
+
+		// start a process
+		loadProcessFromElf(0, dataBuff);
+		free(dataBuff);
+	}
 
 	uint32_t ipAddr = EthConfigureWithIP(0xC0A80007u); //0xC0A80007u => 192.168.0.7
 
 	if (0 != ipAddr) {
 		printf("start listening\n");
-		SchedulerStartProcess(Eth);
 		// init timer
 		TimerConfiguration(Timer_TIMER2, 1000, SchedulerRunNextProcess);
 		TimerEnable(Timer_TIMER2);
